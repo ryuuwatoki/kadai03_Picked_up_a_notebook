76ec2b925f41ac97b3ade0328fcc5d2f112afc6d
ryuuwatoki <140773121+ryuuwatoki@users.noreply.github.com>
2023-11-10 02:44:22 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,109 @@
+var clickSound = new Audio('sound/Death_Note_Ost_1_20.mp3')
+var clickSound2 = new Audio('sound/DeathNoteOP.mp3')
+
+
+
+
+
+$(document).ready(function() {
+    const linkButton = $("#link_div");
+
+    function toggleFade() {
+        linkButton.fadeOut(800, function() {
+            // 在淡出完成后执行淡入
+            linkButton.fadeIn(800);
+        });
+    }
+
+    // 使用 setInterval 创建fade in和fade out效果，间隔1秒
+    setInterval(toggleFade, 1000);
+});
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+
+
+    // 点击 link_div 时触发
+    $("#link_div").on("click", function() {
+
+        clickSound.play();
+
+
+        // 淡出 opening 内的所有元素
+        $("#opening").fadeOut(1000, function() {
+            // 清空 opening
+            $("#opening").empty();
+
+            // n秒后淡入 second 元素
+            setTimeout(function() {
+                $("#second").fadeIn(2000);
+            }, 2000);
+
+            // n秒后淡入 second2 元素
+            setTimeout(function() {
+                $("#second2").fadeIn(2000);
+            }, 4000);
+
+            // n秒后淡入 second3 元素
+            setTimeout(function() {
+                $("#second3").fadeIn(2000);
+            }, 6000);
+
+            // n秒后淡入 second4 元素
+            setTimeout(function() {
+                $("#second4").fadeIn(2000);
+            }, 8000);
+            
+            // n秒后淡入 second5 元素
+            setTimeout(function() {
+                $("#second5").fadeIn(2000);
+            }, 10000);
+
+
+            // n秒后淡入 second6 元素
+            setTimeout(function() {
+                $("#second6").fadeIn(2000);
+            }, 12000);
+
+
+
+            // n秒后淡入 second_next 元素
+            setTimeout(function() {
+                $("#second_next").fadeIn(2000);
+            }, 14000);
+
+
+
+
+        });
+    });
+});
+
+
+
+$(document).ready(function() {
+    $("#second_next").click(function(e) {
+        e.preventDefault(); // 阻止默认链接行为
+
+        clickSound2.play(); // 播放声音
+
+                // 隐藏 #second_box 元素，持续时间为 3 秒
+                $("#second_box").fadeOut(3000);
+
+
+
+        // 在声音结束后，延迟跳转到 index2.html
+        clickSound2.addEventListener("ended", function() {
+            setTimeout(function() {
+                window.location.href = "html/note.html";
+            }, 0); // 延迟0毫秒以确保声音播放结束后立即跳转
+        });
+    });
+});
---
+++ js/note.js
@@ -0,0 +1,143 @@
+var killSound1 = new Audio('../sound/01_first-blood-sound.mp3')
+var killSound2 = new Audio('../sound/02_double-kill-lol.mp3')
+var killSound3 = new Audio('../sound/03_triple-kill-lol.mp3')
+var killSound4 = new Audio('../sound/04_quadrakill-lol.mp3')
+var killSound5 = new Audio('../sound/05_pentakill-lol.mp3')
+var killSound6 = new Audio('../sound/06_legendary.mp3')
+
+var rebornSound1 = new Audio('../sound/12_revival.mp3')
+var rebornAllSound1 = new Audio('../sound/11_hallelujah.mp3')
+
+
+
+
+
+
+$(document).ready(function() {
+    $('body').hide().fadeIn(1000); // 隱藏body，然後在n秒內淡入
+});
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+
+    // Save 按鈕點擊事件
+    $("#save").on("click", function () {
+        const key = $("#title").val();
+        const value = $("#text").val().trim() === '' ? '心臓麻痺' : $("#text").val();
+
+        if (key === '') {
+            alert('名前を入力してください！');
+            return;
+        }
+
+        if (key === '夜神月') {
+            alert('夜神月は神様だから、死にません！');
+            $('#title').val('');
+            $('#text').val('');    
+            return;
+        }
+
+
+        if (localStorage.length === 0) {
+            killSound1.play();
+        }
+
+        if (localStorage.length === 1) {
+            killSound2.play();
+        }
+
+        if (localStorage.length === 2) {
+            killSound3.play();
+        }
+
+        if (localStorage.length === 3) {
+            killSound4.play();
+        }
+
+        if (localStorage.length === 4) {
+            killSound5.play();
+        }
+
+        if (localStorage.length >= 5) {
+            killSound6.play();
+        }
+
+
+
+
+
+        // 將資料存入 localStorage
+        localStorage.setItem(key, value);
+
+        // 生成 HTML 元素並顯示在列表中
+        const html = `
+        <li>
+            <p>${key}</p>
+            <p>${value}</p>
+            <button class="Reborn" data-key="${key}">蘇る</button>
+        </li>
+        `;
+        $("#list").append(html);
+
+        // 清空欄位
+        $('#title').val('');
+        $('#text').val('');
+    });
+
+
+
+
+
+
+
+
+
+    // Clear 按鈕點擊事件
+    $("#clear").on("click", function () {
+
+        if (localStorage.length !==0) {
+            rebornAllSound1.play();
+        }
+        else {
+            alert('犠牲者がいない！');
+        }
+
+        // 清空 localStorage 和列表
+        localStorage.clear();
+        $("#list").empty();
+
+    });
+
+    // 列表中的蘇る按鈕點擊事件
+    $("#list").on("click", ".Reborn", function () {
+        const key = $(this).data("key");
+        rebornSound1.play();
+
+        // 移除相應的值
+        localStorage.removeItem(key);
+
+        // 移除包含該按鈕的 <li> 元素
+        $(this).closest("li").remove();
+    });
+
+    // 頁面載入時顯示 localStorage 中的資料
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        const html = `
+        <li>
+            <p>${key}</p>
+            <p>${value}</p>
+            <button class="Reborn" data-key="${key}">蘇る</button>
+        </li>
+        `;
+        $("#list").append(html);
+    }
+});
+
